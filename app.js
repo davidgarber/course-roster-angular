4843bcf3d2aaafae28fdbf36ab6523e13e0ca499
David Garber <davidgarber@gmail.com>
2015-10-08 10:17:40 -0700

    Add partials/home.html page.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var courseRoster = angular.module('courseRoster', ['ui.router']);
+
+courseRoster.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateURL: "partials/home.html"
+  });
+});
